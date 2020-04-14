bb4609435d4d65df4aff8f1e47748d2fe56b5a77
MH4GF <rixi436@gmail.com>
2020-04-14 23:42:53 +0900

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # == Route Map
 #
-# You don't have any routes defined!
-#
-# Please add some routes in config/routes.rb.
-#
-# For more information about routes, see the Rails guide: https://guides.rubyonrails.org/routing.html.
+#   Prefix Verb   URI Pattern             Controller#Action
+# v1_todos POST   /v1/todos(.:format)     v1/todos#create
+#  v1_todo DELETE /v1/todos/:id(.:format) v1/todos#destroy
+# v1_users GET    /v1/users(.:format)     v1/users#index
+#          POST   /v1/users(.:format)     v1/users#create
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    resources :todos, only: [:create, :destroy]
+    resources :users, only: [:index, :create]
+  end
 end
