feeed55244b5eeda40bda385d04fb2871fbe580f
MH4GF <rixi436@gmail.com>
2020-04-15 00:54:48 +0900

    add users_controller

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -2,5 +2,25 @@
 
 module V1
   class UsersController < ApplicationController
+    def index
+      users = User.all
+      render json: users, each_serializer: UserSerializer
+    end
+
+    def show
+      current_user = User.find(params[:id])
+      render json: current_user, serializer: UserSerializer
+    end
+
+    def create
+      user = User.create!(user_params)
+      render json: user, serializer: UserSerializer, status: :created
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :uid, :name)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@
 #  v1_todo DELETE /v1/todos/:id(.:format) v1/todos#destroy
 # v1_users GET    /v1/users(.:format)     v1/users#index
 #          POST   /v1/users(.:format)     v1/users#create
+#  v1_user GET    /v1/users/:id(.:format) v1/users#show
 
 Rails.application.routes.draw do
   namespace :v1 do
     resources :todos, only: [:create, :destroy]
-    resources :users, only: [:index, :create]
+    resources :users, only: [:index, :show, :create]
   end
 end
